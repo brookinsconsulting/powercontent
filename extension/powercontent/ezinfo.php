0b524b28c085d8202e8b4d43dbf493fd389553ad
Kristof Coomans <kristof.coomans@telenet.be>
2006-12-05 13:10:58 +0000

    extension info file for eZ publish 3.9

---
+++ extension/powercontent/ezinfo.php
@@ -0,0 +1,15 @@
+<?php
+class powercontentInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "Powercontent",
+            'Version' => "0.x",
+            'Copyright' => "Copyright (C) 2006 SCK-CEN",
+            'Author' => "Kristof Coomans",
+            'License' => "GNU General Public License v2.0"
+        );
+    }
+}
+?>

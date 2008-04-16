edd46c1115b869e67fbaf87a79953b3460c41711
Kristof Coomans <kristof.coomans@telenet.be>
2008-04-16 07:26:02 +0000

    - Updated info file

--- extension/powercontent/ezinfo.php
+++ extension/powercontent/ezinfo.php
@@ -5,8 +5,8 @@ class powercontentInfo
     {
         return array(
             'Name' => "Powercontent",
-            'Version' => "0.x",
-            'Copyright' => "Copyright (C) 2006 SCK-CEN",
+            'Version' => "1.x",
+            'Copyright' => "Copyright (C) 2006-2007 SCK-CEN",
             'Author' => "Kristof Coomans",
             'License' => "GNU General Public License v2.0"
         );
